74155025a538d0ba90975eac52442259ba41415a
Lucas Engleder <lucasengleder@gmail.com>
2020-10-19 10:32:26 +0200

    prettier and eslint support

--- components/SimpleTextInput.tsx
+++ components/SimpleTextInput.tsx
@@ -1,77 +1,78 @@
-import React, { useState } from "react";
-import styled from "styled-components";
+import React, { useState } from "react"
 import {
-  StyleProp,
-  StyleSheet,
-  TextInput,
-  TextInputProps,
-  TextStyle,
-  View,
-  ViewStyle,
-} from "react-native";
-import { globalVariables } from "../GlobalStyles";
+    StyleProp,
+    StyleSheet,
+    TextInput,
+    TextInputProps,
+    TextStyle,
+    View,
+    ViewStyle,
+} from "react-native"
+import { globalVariables } from "../GlobalStyles"
 
 interface SimpleTextInputProps {
-  backgroundColor?: string;
-  width?: number;
-  height?: number;
-  borderBottomWidth?: number;
-  borderBottomColor?: string;
-  margin?: number;
-  cursorColor?: string;
-  wrapperStyle?: StyleProp<ViewStyle>;
+    backgroundColor?: string
+    width?: number
+    height?: number
+    borderBottomWidth?: number
+    borderBottomColor?: string
+    margin?: number
+    cursorColor?: string
+    wrapperStyle?: StyleProp<ViewStyle>
 }
 
 export default function SimpleTextInput({
-  width = 260,
-  height = 45,
-  backgroundColor,
-  margin = 3,
-  ...props
+    width = 260,
+    height = 45,
+    backgroundColor,
+    margin = 3,
+    ...props
 }: SimpleTextInputProps & TextInputProps) {
-  const [isFocused, setIsFocused] = useState(false);
+    const [isFocused, setIsFocused] = useState(false)
 
-  const onBlur = () => {
-    // setInputStyle({});
-    setIsFocused(false);
-  };
+    const onBlur = () => {
+        // setInputStyle({});
+        setIsFocused(false)
+    }
 
-  const onFocus = () => {
-    // setInputStyle({ width: width, height: height });
-    setIsFocused(true);
-  };
+    const onFocus = () => {
+        // setInputStyle({ width: width, height: height });
+        setIsFocused(true)
+    }
 
-  const styles = StyleSheet.create({
-    container: {
-      margin: margin,
-    },
-    simple: {
-      width: width,
-      height: height,
-      color: globalVariables.light,
-      backgroundColor,
-      fontFamily: "Montserrat_400Regular",
-      paddingHorizontal: 20,
-      borderBottomColor: globalVariables.light,
-      borderBottomWidth: 3,
-    },
-    focused: {
-      borderBottomColor: globalVariables.primaryOne,
-    },
-  });
+    const styles = StyleSheet.create({
+        container: {
+            margin: margin,
+        },
+        simple: {
+            width: width,
+            height: height,
+            color: globalVariables.light,
+            backgroundColor,
+            fontFamily: "Montserrat_400Regular",
+            paddingHorizontal: 20,
+            borderBottomColor: globalVariables.light,
+            borderBottomWidth: 3,
+        },
+        focused: {
+            borderBottomColor: globalVariables.primaryOne,
+        },
+    })
 
-  // const TextInput = styled.textInpt``;
+    // const TextInput = styled.textInpt``;
 
-  return (
-    <View style={[styles.container, props.wrapperStyle]}>
-      <TextInput
-        style={[styles.simple, isFocused ? styles.focused : null]}
-        placeholderTextColor={isFocused ? globalVariables.primaryOne : "#fff"}
-        onBlur={() => onBlur()}
-        onFocus={() => onFocus()}
-        selectionColor={props.cursorColor}
-        {...props}
-      />
-    </View>
-  );
+    return (
+        <View style={[styles.container, props.wrapperStyle]}>
+            <TextInput
+                style={[styles.simple, isFocused ? styles.focused : null]}
+                placeholderTextColor={
+                    isFocused ? globalVariables.primaryOne : "#fff"
+                }
+                onBlur={() => onBlur()}
+                onFocus={() => onFocus()}
+                selectionColor={props.cursorColor}
+                {...props}
+            />
+        </View>
+    )
 }
