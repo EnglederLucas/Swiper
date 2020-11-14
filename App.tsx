feee71af5c171cf2dae17ec4ba6e43f8779ac2cb
JanGruber <jan_gruber@outlook.com>
2020-11-14 17:28:38 +0100

    SignUp and Login

--- App.tsx
+++ App.tsx
@@ -7,10 +7,12 @@ import SwipePremade from "./src/swipe/SwipePremade";
 import { registerRootComponent } from "expo";
 import DetailView from "./src/swipe/DetailView";
 import { ImageSourcePropType } from "react-native";
+import SignUp from "./src/login/SignUp";
 
 export type AuthenticationStackParameterList = {
   Swipe: undefined;
   Login: undefined;
+  SignUp: undefined;
   Details: { image: ImageSourcePropType };
 };
 
@@ -19,36 +21,37 @@ export const AuthenticationStack = createStackNavigator<
 >();
 
 const AuthenticationNavigator = () => {
-
-  const [user, setUser] = useState(null)
+  const [user, setUser] = useState(null);
 
   //TODO:Add Navbar
   return (
-      <AuthenticationStack.Navigator headerMode="none">
-        {
-          user ? (
-            <>
-            <AuthenticationStack.Screen
-              name="Swipe"
-              component={SwipePremade}></AuthenticationStack.Screen>
-            <AuthenticationStack.Screen
-              name="Details"
-              component={DetailView}
-              initialParams={{
-                image: {
-                  uri:
-                    "https://image.tmdb.org/t/p/original/uTylq8v3lfMUHMt3n0Tb4bA9CtG.jpg",
-                },
-              }}></AuthenticationStack.Screen>
-          </>
-          ) : (
-            <AuthenticationStack.Screen
-              name="Login"
-              component={Login}
-            ></AuthenticationStack.Screen>
-          )
-        }
-      </AuthenticationStack.Navigator>
+    <AuthenticationStack.Navigator headerMode="none">
+      {user ? (
+        <>
+          <AuthenticationStack.Screen
+            name="Swipe"
+            component={SwipePremade}></AuthenticationStack.Screen>
+          <AuthenticationStack.Screen
+            name="Details"
+            component={DetailView}
+            initialParams={{
+              image: {
+                uri:
+                  "https://image.tmdb.org/t/p/original/uTylq8v3lfMUHMt3n0Tb4bA9CtG.jpg",
+              },
+            }}></AuthenticationStack.Screen>
+        </>
+      ) : (
+        <>
+          <AuthenticationStack.Screen
+            name="Login"
+            component={Login}></AuthenticationStack.Screen>
+          <AuthenticationStack.Screen
+            name="SignUp"
+            component={SignUp}></AuthenticationStack.Screen>
+        </>
+      )}
+    </AuthenticationStack.Navigator>
   );
 };
 
